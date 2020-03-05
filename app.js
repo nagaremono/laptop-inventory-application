1baa44156a2ab3ed37cdc682ebe3cd67193c51e9
Guruh Edi Purwanto <guruhedipurwanto@gmail.com>
2020-03-06 04:14:14 +0800

    Added node version for heroku deployment

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(helmet())
 
 //Set up mongoose connection
 var mongoose = require('mongoose')
-var mongoDB =
+var dev_db_url =
   'mongodb+srv://gedip:rawasari135@cluster0-bmkjd.mongodb.net/laptop_inventory?retryWrites=true&w=majority'
+var mongoDB = process.env.MONGODB_URI || dev_db_url
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 var db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
